4882d7b9b0e717f588cb2d475084bb9dd486276c
Andy Taylor <andytlr@gmail.com>
2014-04-07 08:17:32 +1000

    Use reps x kg rather than kg x reps

--- 5-3-1.rb
+++ 5-3-1.rb
@@ -30,115 +30,115 @@ output <<
 "| #{bp100}kg     | #{bs100}kg     | #{dl100}kg     |\n\n"\
 "## Week 1\n\n"\
 "### Monday: Bench Press\n\n"\
-"- [ ] #{65.po(bp_orm)}kg × 5\n"\
-"- [ ] #{75.po(bp_orm)}kg × 5\n"\
-"- [ ] #{85.po(bp_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{65.po(bp_orm)}kg\n"\
+"- [ ] 5 × #{75.po(bp_orm)}kg\n"\
+"- [ ] 5 × #{85.po(bp_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n\n"\
 "### Wednesday: Back Squat\n\n"\
-"- [ ] #{65.po(bs_orm)}kg × 5\n"\
-"- [ ] #{75.po(bs_orm)}kg × 5\n"\
-"- [ ] #{85.po(bs_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{65.po(bs_orm)}kg\n"\
+"- [ ] 5 × #{75.po(bs_orm)}kg\n"\
+"- [ ] 5 × #{85.po(bs_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n\n"\
 "### Friday: Deadlift\n\n"\
-"- [ ] #{65.po(dl_orm)}kg × 5\n"\
-"- [ ] #{75.po(dl_orm)}kg × 5\n"\
-"- [ ] #{85.po(dl_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{65.po(dl_orm)}kg\n"\
+"- [ ] 5 × #{75.po(dl_orm)}kg\n"\
+"- [ ] 5 × #{85.po(dl_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n\n"\
 "## Week 2\n\n"\
 "### Monday: Bench Press\n\n"\
-"- [ ] #{70.po(bp_orm)}kg × 3\n"\
-"- [ ] #{80.po(bp_orm)}kg × 3\n"\
-"- [ ] #{90.po(bp_orm)}kg × 3 + AMRAP\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n\n"\
+"- [ ] 3 × #{70.po(bp_orm)}kg\n"\
+"- [ ] 3 × #{80.po(bp_orm)}kg\n"\
+"- [ ] 3 × #{90.po(bp_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n\n"\
 "### Wednesday: Back Squat\n\n"\
-"- [ ] #{70.po(bs_orm)}kg × 3\n"\
-"- [ ] #{80.po(bs_orm)}kg × 3\n"\
-"- [ ] #{90.po(bs_orm)}kg × 3 + AMRAP\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n\n"\
+"- [ ] 3 × #{70.po(bs_orm)}kg\n"\
+"- [ ] 3 × #{80.po(bs_orm)}kg\n"\
+"- [ ] 3 × #{90.po(bs_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n\n"\
 "### Friday: Deadlift\n\n"\
-"- [ ] #{70.po(dl_orm)}kg × 3\n"\
-"- [ ] #{80.po(dl_orm)}kg × 3\n"\
-"- [ ] #{90.po(dl_orm)}kg × 3 + AMRAP\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n\n"\
+"- [ ] 3 × #{70.po(dl_orm)}kg\n"\
+"- [ ] 3 × #{80.po(dl_orm)}kg\n"\
+"- [ ] 3 × #{90.po(dl_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n\n"\
 "## Week 3\n\n"\
 "### Monday: Bench Press\n\n"\
-"- [ ] #{75.po(bp_orm)}kg × 5\n"\
-"- [ ] #{85.po(bp_orm)}kg × 3\n"\
-"- [ ] #{95.po(bp_orm)}kg × 1 + AMRAP\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{75.po(bp_orm)}kg\n"\
+"- [ ] 3 × #{85.po(bp_orm)}kg\n"\
+"- [ ] 1 × #{95.po(bp_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n\n"\
 "### Wednesday: Back Squat\n\n"\
-"- [ ] #{75.po(bs_orm)}kg × 5\n"\
-"- [ ] #{85.po(bs_orm)}kg × 3\n"\
-"- [ ] #{95.po(bs_orm)}kg × 1 + AMRAP\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{75.po(bs_orm)}kg\n"\
+"- [ ] 3 × #{85.po(bs_orm)}kg\n"\
+"- [ ] 1 × #{95.po(bs_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n\n"\
 "### Friday: Deadlift\n\n"\
-"- [ ] #{75.po(dl_orm)}kg × 5\n"\
-"- [ ] #{85.po(dl_orm)}kg × 3\n"\
-"- [ ] #{95.po(dl_orm)}kg × 1 + AMRAP\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{75.po(dl_orm)}kg\n"\
+"- [ ] 3 × #{85.po(dl_orm)}kg\n"\
+"- [ ] 1 × #{95.po(dl_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n\n"\
 "## Week 4\n\n"\
 "### Monday: Bench Press\n\n"\
-"- [ ] #{40.po(bp_orm)}kg × 5\n"\
-"- [ ] #{50.po(bp_orm)}kg × 5\n"\
-"- [ ] #{60.po(bp_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n"\
-"- [ ] #{50.po(bp_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{40.po(bp_orm)}kg\n"\
+"- [ ] 5 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 5 × #{60.po(bp_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bp_orm)}kg\n\n"\
 "### Wednesday: Back Squat\n\n"\
-"- [ ] #{40.po(bs_orm)}kg × 5\n"\
-"- [ ] #{50.po(bs_orm)}kg × 5\n"\
-"- [ ] #{60.po(bs_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n"\
-"- [ ] #{50.po(bs_orm)}kg × 10\n\n"\
+"- [ ] 5 × #{40.po(bs_orm)}kg\n"\
+"- [ ] 5 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 5 × #{60.po(bs_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n"\
+"- [ ] 10 × #{50.po(bs_orm)}kg\n\n"\
 "### Friday: Deadlift\n\n"\
-"- [ ] #{40.po(dl_orm)}kg × 5\n"\
-"- [ ] #{50.po(dl_orm)}kg × 5\n"\
-"- [ ] #{60.po(dl_orm)}kg × 5 + AMRAP\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
-"- [ ] #{50.po(dl_orm)}kg × 10\n"\
+"- [ ] 5 × #{40.po(dl_orm)}kg\n"\
+"- [ ] 5 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 5 × #{60.po(dl_orm)}kg + AMRAP\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
+"- [ ] 10 × #{50.po(dl_orm)}kg\n"\
 
 output.close
