e43e5f9714c3d9d4d717ddd32884e826e58ef12c
Andy Taylor <andytlr@gmail.com>
2014-04-08 09:47:26 +1000

    better specs

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -3,14 +3,19 @@ require_relative '../lib/helpers'
 describe Numeric do
 
   describe "#round_to_increment_of" do
-    it "rounds 66.5 to an increment of 2.5" do
+    it "rounds a number to an increment of another number" do
       66.5.round_to_increment_of(2.5).should eql 67.5
+      66.round_to_increment_of(2.5).should eql 65.0
+      65.round_to_increment_of(2.5).should eql 65.0
     end
   end
 
   describe "#percent_of_rounded" do
     it "gets x percentage of input and rounds to the nearest 2.5" do
       65.percent_of_rounded(88).should eql "57.5"
+      65.percent_of_rounded(88).should_not eql "57.2"
+      30.percent_of_rounded(20).should eql "5"
+      30.percent_of_rounded(20).should_not eql "6"
     end
 
     it "trims trailing decimals if 0" do
@@ -22,6 +27,7 @@ describe Numeric do
   describe "#percent_of" do
     it "gets a percentage as a floating point number" do
       50.percent_of(100).should eql 50.0
+      60.percent_of(80).should eql 48.0
     end
   end
 
